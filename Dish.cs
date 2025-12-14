7f9980ff00c8c904239a6d6a7bc689cfd42674db
JAM NONO <pearljamcxy@gmail.com>
2025-12-15 01:52:35 +0900

    summit

---
+++ Dish.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Dish
+    {
+        public string Name;
+        public int Price;
+        public double CookTime;
+
+        //构造函数
+        public Dish(string name, int price, double cooktime)
+        {
+            Name = name;
+            Price = price;
+            CookTime = cooktime;
+            DateTime BrokenTime = DateTime.Now.AddMinutes(2);
+
+
+            Console.WriteLine($"You have cooked dish{Name}");
+        }
+
+
+    }
+}
