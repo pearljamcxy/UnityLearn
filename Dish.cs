98f8a869c8008a507f0767dfb25c9657343f6f0d
JAM NONO <pearljamcxy@gmail.com>
2025-12-15 22:50:28 +0900

    summit

--- Dish.cs
+++ Dish.cs
@@ -2,28 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Dish
+    //先把架构搭出来，再往里填东西
+    //===== 材料定义 =====
+    enum Ingredient
     {
-        public string Name;
-        public int Price;
-        public double CookTime;
+        Bread,
+        Bacon,
+        Beef,
+        Cheese,
+        Carrot,
+        Lettuce
 
-        //构造函数
-        public Dish(string name, int price, double cooktime)
+    }
+    //===== 菜谱 =====
+    public class Dish
+    {
+        internal string Name;
+        internal int Price;
+        internal int CookTime;
+        internal int SpoilTime;
+        //这个 List 里只能放 enum Ingredients 的值：
+        //我有一个变量，名字叫 RequiredIngredients，它是一个装着 Ingredients（枚举）的 List。
+        internal List<Ingredient> RequiredIngredients;
+
+        internal Dish(string name,
+            int price,
+            int cookTime,
+            int spoilTime,
+            List<Ingredient> requiredIngredients)
         {
             Name = name;
             Price = price;
-            CookTime = cooktime;
-            DateTime BrokenTime = DateTime.Now.AddMinutes(2);
-
-
-            Console.WriteLine($"You have cooked dish{Name}");
+            CookTime = cookTime;
+            SpoilTime = spoilTime;
+            RequiredIngredients = requiredIngredients;
         }
+     }
 
+    //===== 厨房 =====
+    public static class Kitchen
+    {
 
     }
+
+    public static class Player
+    {
+
+    }
+    
 }
--- MainSwitch.cs
+++ MainSwitch.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -9,17 +10,78 @@ namespace ConsoleApp1
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("=====choose your practice=======!!");
 
-            // FarmGame game = new FarmGame();  // 创建实例
-            // game.Start();                    // 调用实例方法
+            /*
+            ==============================种田游戏入口=========================================
+            FarmGame game = new FarmGame();  // 创建实例
+            game.Start();                    // 调用实例方法
+            */
 
-            // SlimeGauntletTest.MainTest2();
+            /*
+            ==============================史莱姆游戏入口=========================================
+            SlimeGauntletTest.MainTest2();
+            */
 
+
+            /* 
+            ==============================星球游戏入口=========================================
             Planet earth = new Planet("Earth", 2.0);
             for (int i = 0; i < 10; i++)
             {
                 earth.Tick(1.0);
                 Console.WriteLine($"Time {i+1}s ----- Energy:{earth.Energy}");
             }
+            */
+
+
+            //==============================做饭游戏入口=========================================
+            //定义三种dish
+            Dish burgerKing = new Dish(
+                "BurgerKing", 400, 10, 10,
+                new List<Ingredient>
+                {
+                    Ingredient.Bread,
+                    Ingredient.Bacon,
+                    Ingredient.Beef,
+                    Ingredient.Cheese,
+                    Ingredient.Carrot,
+                    Ingredient.Lettuce
+                }
+             );
+
+            Dish baconCheeseBurger = new Dish(
+                "BaconCheeseBurger",
+                250,
+                7,
+                10,
+                new List<Ingredient>
+                {
+                    Ingredient.Cheese,
+                    Ingredient.Bread,
+                    Ingredient.Bacon,
+                }
+             );
+
+            Dish veggieBurger = new Dish(
+                "VeggieBurger",
+                200,
+                5,
+                10,
+                new List<Ingredient>
+                {
+                    Ingredient.Bread,
+                    Ingredient.Lettuce,
+                    Ingredient.Carrot,
+                    Ingredient.Cheese
+                }
+             );
+
+
+
+
+
         }
+
+
     }
+
 }
